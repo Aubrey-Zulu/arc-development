b789146b59893380981a3839557de091cd3c1773
Aubrey Zulu <aubreydarious@gmail.com>
2021-02-07 09:22:50 +0200

    add theme, customise the paletter

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
+import { ThemeProvider } from '@material-ui/styles';
 import Header from './ui/Header';
+import theme from './ui/Theme';
 
 function App() {
   return (
-    <div>
-      <Header />
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join('\n')}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Header />
+        hello
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { AppBar, Toolbar, useScrollTrigger } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const ElevationScroll = (props) => {
   const { children, window } = props;
@@ -15,13 +21,25 @@ const ElevationScroll = (props) => {
   });
 };
 
+const useStyles = makeStyles((theme) => ({
+  toolBarMargin: {
+    ...theme.mixins.toolbar,
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>Arc development</Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h3">Arc development</Typography>{' '}
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolBarMargin} />
+    </>
   );
 };
 
