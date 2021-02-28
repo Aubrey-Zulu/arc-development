859e367e6ec2b4645969693aa2d20547563f2bca
Aubrey Zulu <aubreydarious@gmail.com>
2021-02-28 11:48:40 +0200

    add navigation bar, routes and theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import Header from './ui/Header';
 import theme from './ui/Theme';
@@ -5,10 +6,32 @@ import theme from './ui/Theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
+      <BrowserRouter>
         <Header />
-        hello
-      </div>
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/custom-software"
+            component={() => <div>custom software</div>}
+          />
+          <Route
+            exact
+            path="/mobile-apps"
+            component={() => <div>mobile apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About</div>} />
+          <Route exact path="/contact" component={() => <div>contact</div>} />
+          <Route exact path="/estimate" component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   AppBar,
   Toolbar,
-  Typography,
   useScrollTrigger,
+  Tabs,
+  Tab,
+  Button,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
 
 const ElevationScroll = (props) => {
   const { children, window } = props;
@@ -24,17 +28,117 @@ const ElevationScroll = (props) => {
 const useStyles = makeStyles((theme) => ({
   toolBarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: '3em',
+  },
+  logo: { height: '7em' },
+  tabContainer: {
+    marginLeft: 'auto',
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px',
+    color: 'white',
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: '50px',
+    marginLeft: '25px',
+    marginRight: '25px',
+    height: '45px',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const { pathname } = useLocation();
+
+  const [value, setValue] = useState(0);
+  const handleChange = (event, value) => {
+    setValue(value);
+  };
+
+  // persist navigation Url after reload
+  useEffect(() => {
+    if (pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [pathname]);
+
   return (
     <>
       <ElevationScroll>
         <AppBar>
-          <Toolbar>
-            <Typography variant="h3">Arc development</Typography>{' '}
+          <Toolbar disableGutters>
+            <Button
+              disableRipple
+              className={classes.logoContainer}
+              component={Link}
+              to="/"
+            >
+              <img className={classes.logo} src={logo} alt="company logo" />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                label="Home"
+                component={Link}
+                to="/"
+              />
+              <Tab
+                className={classes.tab}
+                label="Services"
+                component={Link}
+                to="/services"
+              />
+              <Tab
+                className={classes.tab}
+                label="The revolution"
+                component={Link}
+                to="/revolution"
+              />
+              <Tab
+                component={Link}
+                to="/about"
+                className={classes.tab}
+                label="About us"
+              />
+              <Tab
+                className={classes.tab}
+                label="Contact us"
+                component={Link}
+                to="contact"
+              />
+            </Tabs>
+            <Button
+              color="secondary"
+              className={classes.button}
+              disableElevation
+              variant="contained"
+            >
+              Free estimate
+            </Button>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
